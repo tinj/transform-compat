e4705187c9e8efe3d4e13f35ae4b43a408d20490
Mat Tyndall <mat.tyndall@gmail.com>
2014-12-02 21:33:41 -0800

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ transform-compat
 ================
 
 test browser transform compatibility
+
+intended to be used via browserify
---
+++ compat.js
@@ -0,0 +1,32 @@
+// Feature detection and compatibility hacks
+
+var testResults = {};
+
+// http://stackoverflow.com/questions/5661671/detecting-transform-translate3d-support
+testResults.csstransforms3d = setTimeout(function() {
+    var el = document.createElement('p'),
+        has3d,
+        transforms = {
+            'webkitTransform':'-webkit-transform',
+            'OTransform':'-o-transform',
+            'msTransform':'-ms-transform',
+            'MozTransform':'-moz-transform',
+            'transform':'transform'
+        };
+
+    // Add it to the body to get the computed style.
+    document.body.insertBefore(el, null);
+
+    for (var t in transforms) {
+        if (el.style[t] !== undefined) {
+            el.style[t] = 'translate3d(1px,1px,1px)';
+            has3d = window.getComputedStyle(el).getPropertyValue(transforms[t]);
+        }
+    }
+
+    document.body.removeChild(el);
+
+    return (has3d !== undefined && has3d.length > 0 && has3d !== 'none');
+}, 0);
+
+module.exports = testResults;
